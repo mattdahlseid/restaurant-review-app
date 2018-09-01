7dfd8ba26d66ea4d91079b2883d83adf4f9f099b
Matt-Dahlseid <matt.dahlseid@gmail.com>
2018-09-01 04:04:49 -0700

    Update SW Removed console.log() entries

--- sw.js
+++ sw.js
@@ -34,12 +34,9 @@ const cacheAssets = [
 
 // Call install event
 self.addEventListener('install', (e) => {
-    console.log('Service Worker Installed!');
     e.waitUntil(
-        caches
-            .open(cacheName)
+        caches.open(cacheName)
             .then(cache => {
-                console.log('Service Worker: Caching Files');
                 cache.addAll(cacheAssets);
             })
             .then(() => self.skipWaiting())
@@ -48,14 +45,12 @@ self.addEventListener('install', (e) => {
 
 
 self.addEventListener('activate', e => {
-    console.log('Service Worker Activated');
-    // remove unwanted caches
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
+    // remove old cache if new cache is available
+    e.waitUntil(caches.keys()
+        .then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== cacheName) {
-                        console.log('Service Worker Clearing Old Cache')
                         return caches.delete(cache);
                     }
                 })
@@ -66,6 +61,5 @@ self.addEventListener('activate', e => {
 
 // Call fetch Event
 self.addEventListener('fetch', e => {
-    console.log('Service Worker Fetching');
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });

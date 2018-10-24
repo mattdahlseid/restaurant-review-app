f66c1f525615530b8cf097c2017b3129779ce0eb
Matt-Dahlseid <matt.dahlseid@gmail.com>
2018-10-24 13:02:58 -0700

    Clarified image directory

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    */
 
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
